4e7de5f998c3cd5651279c2d6901f3464e07f28c
programmering <programmering@gr1.gdc>
2018-07-27 22:00:30 +0200

    det lille spil hvor de klodser hopper rundt

---
+++ Spil/Assets/Scripts/Kødbollebevægelsesscript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kødbollebevægelsesscript : MonoBehaviour {
+
+
+    public float MovementSpeed = 5f;
+    public float JumpSpeed = 5f;
+    public int OnGround = 0;
+    public Rigidbody MyRigidbody;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+     
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        /*if (Input.GetKey (KeyCode.D))
+        {
+            MoveHøjre(MovementSpeed);
+
+        } else if (Input.GetKey (KeyCode.A))
+        {
+            MoveHøjre(-MovementSpeed);
+        }
+        */
+        
+        MoveHøjre(MovementSpeed * Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (OnGround == 1)
+            {
+                Jumping(JumpSpeed);
+
+                OnGround = 0;
+            }
+
+       
+        }
+         
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        OnGround = 1;
+    }
+
+    public void Jumping (float JumpHeight)
+    {
+        MyRigidbody.velocity = (Vector3.right * MyRigidbody.velocity.x) + (Vector3.up * JumpHeight); 
+
+    }
+    
+        
+        
+    
+
+    public void MoveHøjre(float Speed)
+    {
+        MyRigidbody.velocity = (Vector3.up * MyRigidbody.velocity.y) + (transform.forward * Speed);
+
+        
+
+
+    }
+}
