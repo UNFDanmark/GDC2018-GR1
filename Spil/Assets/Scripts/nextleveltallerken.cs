d75db78938d209afa4c1d0a0bb5a11bd9631ad68
programmering <programmering@gr1.gdc>
2018-07-28 13:45:10 +0200

    noget kode ændring

---
+++ Spil/Assets/Scripts/nextleveltallerken.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nextleveltallerken : MonoBehaviour {
+
+    public Camera MyCamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        MyCamera.WorldToViewportPoint(Vector3.up * 20);
+           
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Spil/Assets/Scripts/ståfuckingstille.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ståfuckingstille : MonoBehaviour {
+
+    public Rigidbody MyRigidbody;
+    public Vector3 cameraPosition;
+    public GameObject Player;
+    public float PlayerY;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        cameraPosition = new Vector3(31f, PlayerY, 1.7f);
+        transform.position = cameraPosition;
+
+
+        PlayerY = Player.transform.position.y;
+    }
+}
