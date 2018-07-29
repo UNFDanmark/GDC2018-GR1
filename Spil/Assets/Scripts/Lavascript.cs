578f2a7a1421b33cf7866f76d225628dd1828f23
programmering <programmering@gr1.gdc>
2018-07-30 01:31:41 +0200

    Game Over Screen er lavet og virker :))

--- Spil/Assets/Scripts/Lavascript.cs
+++ Spil/Assets/Scripts/Lavascript.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Lavascript : MonoBehaviour {
 
     public Rigidbody Lava;
     public float LavaRising = 0.2f;
+    public int DeathCount = 0;
 
 
 	// Use this for initialization
@@ -18,8 +21,12 @@ public class Lavascript : MonoBehaviour {
         Vector3 ShitFuck = Lava.transform.position;
         Lava.transform.position = Lava.transform.position + Vector3.up * LavaRising * Time.deltaTime;
 
+        if (DeathCount >= 2)
+        {
+            SceneManager.LoadScene("GameOverScreen");
+        }
 
-	}
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -28,12 +35,17 @@ public class Lavascript : MonoBehaviour {
         {
 
             Destroy(other.gameObject);
+
+            DeathCount++;
+
         }
 
-
+        
     }
 
-
+    
+        
+    
 
 }
 
