fdc8b9b676bf0929717f8668fc309811d4226bb4
programmering <programmering@gr1.gdc>
2018-08-01 00:01:41 +0200

    LAST EDIT

--- Spil/Assets/Scripts/RetryScript.cs
+++ Spil/Assets/Scripts/RetryScript.cs
@@ -15,7 +15,7 @@ public class RetryScript : MonoBehaviour {
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 SceneManager.LoadScene("GameScreen");
             }
