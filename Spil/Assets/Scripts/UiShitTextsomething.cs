ad13be65eb94b4f8d11175ae8e563d0f0b284512
programmering <programmering@gr1.gdc>
2018-07-31 10:30:52 +0200

    Gafler med mere, bane

--- Spil/Assets/Scripts/UiShitTextsomething.cs
+++ Spil/Assets/Scripts/UiShitTextsomething.cs
@@ -5,15 +5,26 @@ using UnityEngine.UI;
 
 public class UiShitTextsomething : MonoBehaviour {
 
+    
+    public int Health1 = 3;
+    public int Health2 = 3;
+    public Text HealthShow1;
+    public Text HealthShow2;
+    
+
+    public void LoseHealthPlayer1()
+    {
+        Health1--;
+        HealthShow1.text = "Player 1 HP: " + Health1;
+
+    }
+
+    public void LoseHealthPlayer2()
+    {
+        Health2--;
+        HealthShow2.text = "Player 2 HP: " + Health2;
+
+    }
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
